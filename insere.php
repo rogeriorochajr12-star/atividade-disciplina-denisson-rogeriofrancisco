6cffad4b8d264aa9baa76acd6eb14b9c95437f5d
Rogerio Francisco da Rocha Junior <rogeriorochajr12@gmail.com>
2026-09-03 14:15:51 +0000

    Atualizei as tabelas e consertei o cadastro

--- insere.php
+++ insere.php
@@ -5,14 +5,14 @@ ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_report
 $conexao = mysqli_connect("localhost", "francisco", "silvana00") //porta usuário, senha
 or die("Erro ao conectar com o banco de dados"); //caso não consiga conectar mostra a mensagem de erro mostrada na conexão
 
-$select_db = mysqli_select_db($conexao, "novo"); //seleciona o banco de dados
+$select_db = mysqli_select_db($conexao, "novo_e_belo"); //seleciona o banco de dados
 
 //Abaixo atribuídos os valores provenientes do formulário pelo método POST
-$nome = $_POST['nome'];
-$user = $_POST['user'];
-$email = $_POST['email'];
+$nome = $_POST["nome"];
+$senha = $_POST["senha"];
+$email = $_POST["email"];
 
-$string_sql = "INSERT INTO pessoa (id, nome, user, email) VALUES (null, '$nome', '$user', '$email')"; //comando SQL para inserir
+$string_sql = "INSERT INTO Cadastro (id, nome, senha, email) VALUES (null, '$nome', '$senha', '$email')"; //comando SQL para inserir
 
 mysqli_query($conexao, $string_sql); //Realiza a consulta
 
